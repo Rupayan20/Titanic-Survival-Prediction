36bac5c051349e33d80e210aa2cf66f967a8d770
Rupayan Dutta <102848153+Rupayan20@users.noreply.github.com>
2023-02-21 22:43:04 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,8 @@
+# Required Libraries for the Analysis of the Titanic Disaster
+import pandas as pd
+import numpy as np
+from matplotlib import pyplot as plt
+import seaborn as sns
+
+# Data Preprocessing and Exploratory Data Analysis
+train_df = pd.read_csv("https://drive.google.com/file/d/1_HsxBaBIbWIwEnGRkHhcS0MeJH355xun/view?usp=sharing")
