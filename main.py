1ca4032f690f176cf25f0fe551abfe8b2001ac60
Rupayan Dutta <102848153+Rupayan20@users.noreply.github.com>
2023-03-01 12:09:37 +0530

    Update main.py

--- main.py
+++ main.py
@@ -45,3 +45,61 @@ sns.barplot(
     y = "Survived",
 )
 plt.show()
+
+# Actual dataset prepration begins here. The Data tensors are extracted from the actual training dataset.
+seed = ["PassengerId", "Name", "Ticket", "Cabin", "SibSp", "Parch"]
+df_train_set = train_df.drop(seed, axis=1)
+df_train_set.head(10)
+
+mean_age = df_train_set["Age"].mean()
+mean_fare = df_train_set["Fare"].mean()
+
+df_train_set["Age"], df_train_set["Fare"] = df_train_set["Age"].fillna(mean_age), df_train_set["Fare"].fillna(mean_fare)
+
+df_train_set.info()
+
+from sklearn.preprocessing import LabelEncoder
+encoder = LabelEncoder()
+
+df_train_set.iloc[:, 2] = encoder.fit_transform(
+    df_train_set.iloc[:, 2].values
+)
+df_train_set.iloc[:, 5] = encoder.fit_transform(
+    df_train_set.iloc[:, 5].values
+)
+
+df_train_set.info()
+
+df_train_set.head(10)
+
+X = df_train_set.iloc[:, 1:6].values
+
+y = df_train_set.iloc[:, 0].values
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+print("The Shape of the training data tensor are : {} for the features and {} for the targets.".format(X_train.shape, y_train.shape))
+print("The Shape of the validation data tensor are : {} for the features and {} for the targets.".format(X_test.shape, y_test.shape))
+
+
+# Implementation of the K-Nearest Neighbors Classifier Algorithm for prediction of passenger survivality.
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn import metrics
+from tqdm import tqdm
+
+k_range = range(1, 500)
+scores = {}
+scores_list = []
+for k in tqdm(k_range) :
+    knn_model = KNeighborsClassifier(n_neighbors=k)
+    knn_model.fit(X_train, y_train)
+    y_prediction = knn_model.predict(X_test)
+    scores[k] = metrics.accuracy_score(y_test, y_prediction)
+    scores_list.append(metrics.accuracy_score(y_test, y_prediction))
+    
+    from matplotlib import pyplot as plt
+plt.figure(figsize=(5, 5))
+plt.plot(k_range, scores_list)
+plt.xlabel("Value of K for the KNN Classifier")
+plt.ylabel("Testing Accuracy")
