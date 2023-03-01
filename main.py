20dab0aea08afbbc394fd7d4df3885544ef9e7a4
Rupayan Dutta <102848153+Rupayan20@users.noreply.github.com>
2023-03-01 10:11:34 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,3 +6,17 @@ import seaborn as sns
 
 # Data Preprocessing and Exploratory Data Analysis
 train_df = pd.read_csv("https://drive.google.com/file/d/1_HsxBaBIbWIwEnGRkHhcS0MeJH355xun/view?usp=sharing")
+train_df.head(10)
+targets = train_df.iloc[:, 1]
+print(targets)
+
+corr_matrix = train_df.corr()
+corr_matrix
+
+plt.figure(figsize=(10, 10))
+sns.heatmap(corr_matrix, annot=True, xticklabels=True, yticklabels=True, cmap='inferno')
+corr_matrix["Survived"].sort_values(ascending=False)
+train_df.info()
+
+train_df["Embarked"] = train_df["Embarked"].fillna("S")
+train_df.info()
